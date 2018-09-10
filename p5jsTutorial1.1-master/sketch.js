f2234c1a73331bb51924af98578e1c19704ab3f1
TheMiniCamper <42793353+TheMiniCamper@users.noreply.github.com>
2018-09-10 12:02:33 +0800

    Add files via upload

---
+++ p5jsTutorial1.1-master/sketch.js
@@ -0,0 +1,67 @@
+function setup()
+{
+  //create a drawing service 700px wide, 500px tall
+  //create a canvas with (x,y) x coordinate and y coordinate
+ createCanvas(1500,900);
+  
+}
+
+function draw()
+{
+   
+fill(0,0,0)
+rect(0,0,1500,300)
+fill(255,0,0)
+rect(0,300,1500,300)
+fill(255,255,0)
+rect(0,600,1500,300)
+
+}
+
+
+
+
+
+
+  createCanvas(1500,900);
+
+fill(0,0,0)
+rect(0,0,500,300)
+fill(255,0,0)
+rect(0,300,500,300)
+fill(255,255,0)
+rect(0,600,1500,300)
+
+
+
+
+   //create a background using RGB values
+  background(75,50,100)
+  // change the paint brush to a specific color according to RGB values
+  fill(6,248,255);
+  // rectangle 1 top left corner
+  rect(0,0,200,200);
+  // rectangle 2 top right corner
+  rect(1200,0,200,200)
+  // rectangle 3 bottom left corner
+  rect(0,1600,200,200)
+  // rectangle 4 bottom right corner
+  rect(1200,1600,200,200)
+  //rectangle 5 middle 
+  rect(600,800,200,200)
+
+
+
+
+
+
+  // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
+  rect(10,10,100,50);
+  // change in the x coordinate from 10 to 60
+  rect(60,60,100,50)
+  // change in the y value from 10 to 120 
+  rect(120,120,100,50)
+  // change in the length from 100 to 300
+  rect(180,230,300,50)
+  // change in the height from 50 to 300
+  rect(490,290,300,300)
